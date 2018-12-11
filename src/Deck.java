a9053e5417cb3dfaafbbf5c23771069be8a70f6b
Xander <acox9567@bths.edu>
2018-12-11 10:16:19 -0500

    sdaf

--- src/Deck.java
+++ src/Deck.java
@@ -20,6 +20,7 @@ public class Deck {
         }
     }
 
+
     public boolean isEmpty()
     {
         if (unDealt.size() <= 0)
@@ -28,8 +29,60 @@ public class Deck {
         return false;
     }
 
+
     public int size()
     {
         return unDealt.size();
+    }
+
+
+    public Card deal()
+    {
+        if (isEmpty())
+            return null;
+
+        Card card = unDealt.remove((unDealt.size() - 1));
+
+        Dealt.add(card);
+
+        return card;
+    }
+
+
+    public void shuffle()
+    {
+        unDealt.add(Dealt.remove(0));
+
+        for (int k = 51; k >= 1; k--)
+        {
+            int rand = (int)(Math.random() * k);
+
+            Card placeHolder = unDealt.get(k);
+            unDealt.set(k, unDealt.get(rand));
+            unDealt.set(rand, placeHolder);
+        }
+    }
+
+    public String toString()
+    {
+        String str = "Dealt cards: [";
+
+        for (int i = 0; i < Dealt.size(); i++)
+        {
+            str += Dealt.get(i);
+        }
+
+        str = str.substring(0, str.length() - 2);
+        str += "]\nUndealt card: ";
+
+        for (int i = 0; i < unDealt.size(); i++)
+        {
+            str += unDealt.get(i);
+        }
+
+        str = str.substring(0, str.length() - 2);
+        str += "]";
+
+        return str;
     }
 }
---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+//Alexander Cox
+//Tuesday, December 11, 2018
+
+public class Runner {
+
+    public static void main (String[] args)
+    {
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        String[] suits = {"Diamonds", "Clubs", "Hearts", "Spades"};
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+        Deck deck = new Deck(ranks, suits, values);
+
+        System
+    }
+}
